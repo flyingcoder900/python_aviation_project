f1fb0288786b53055e5f6f773d8bf581a4c0fee3
flyingcoder900 <51812171+flyingcoder900@users.noreply.github.com>
2019-06-13 23:13:47 -0700

    Here is the updates for max_payload instead of hard coded 750. Also added a print message to test whether a commit worked from pycharm.
    Signed-off-by: flyingcoder900 <bryan720b@gmail.com>

--- app.py
+++ app.py
@@ -1,23 +1,25 @@
-## python_aviation_project
-##This is some code that I am putting together while learning python. 
+#  python_aviation_project
+# This is some code that I am putting together while learning python. 
 
 pilot = int(input("Enter Pilot weight: "))
 co_pilot = int(input("Enter Copilot weight: "))
 passengers = int(input("Enter Passengers weight: "))
 fuel = int(input("Enter Gallons: ")) * 6
+max_payload = 850
 
 pay_load = pilot + co_pilot + passengers + fuel
 
 print("Pay load: " + str(pay_load))
+print("This is a commit test change.")
 
-if pay_load == 750:
+if pay_load == max_payload:
     print("You are at max weight.")
     print("Be careful with high density altitude at Max weight. Have a safe flight and squawk VFR!")
-elif pay_load <= 750:
-    print("You're under weight by " + str(750 - pay_load) + " pounds.")
+elif pay_load <= max_payload:
+    print("You're under weight by " + str(max_payload - pay_load) + " pounds.")
     print("Have a safe flight and squawk VFR!")
 else:
-    print("You're OVER weight by " + str(pay_load - 750) + "pounds.")
-    print("You will need to take " + str((pay_load - 750) / 6) + " less gallons of fuel.")
+    print("You're OVER weight by " + str(pay_load - max_payload) + " pounds.")
+    print("You will need to take " + str((pay_load - max_payload) / 6) + " less gallons of fuel.")
 
 

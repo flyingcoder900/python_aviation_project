f9a6ed84943e81de9bfc32d924f89bc149e0c264
flyingcoder900 <bryan720b@gmail.com>
2019-06-14 23:31:05 -0700

    These are some changes made on 6_24.

--- app.py
+++ app.py
@@ -1,48 +1,62 @@
 #  python_aviation_project
-# This is some code that I am putting together while learning python.
 
-# set up the show.
+# Que the music.
 
-plane_models = ["C172", "PA28R", "C150"]
+plane_models = ["C172M", "PA28R", "C150"]
 plane_payloads = [650, 900, 500]
 
 
 print("Plane models: " + str(plane_models))
-plane = input("Enter plane Model: ")
+plane = input("Enter plane ICAO + Model Number : ")
+
+# This will set the payload if the plane is already in the system. Or add the plane and payload if it is not already.
+
+if plane in plane_models:
+    max_payload = plane_payloads.pop(plane_models.index(plane))
+    print("Your plane is set as a " + plane + ". With a max payload of " + str(max_payload) )
 if plane not in plane_models:
     print("The plane was not listed!")
     new_plane = plane
     plane_payloads.append(input("What is the payload of this plane: "))
+    max_payload = int(plane_payloads[-1])
     plane_models.append(new_plane)
-    print(plane_models)
+    print("Your plane is set as a " + plane_models[-1] + ". With a payload of " + str(plane_payloads[-1] ) )
+
+# Request User to enter weights for flight.
 
 pilot = int(input("Enter Pilot weight: "))
 co_pilot = int(input("Enter Copilot weight: "))
 passengers = int(input("Enter Passengers weight: "))
 fuel = int(input("Enter Gallons: "))
 
+# Add up the weights
+
 fuel_weight = fuel * 6
-max_payload = plane_payloads [1]
-over_weight = False
+over_weight = True
 pay_load = pilot + co_pilot + passengers + fuel_weight
 excess_weight = pay_load - max_payload
 fuel_removed = ((pay_load - max_payload) / 6)
 
+# Final checks to make sure pilot is at appropriate weight.
 
 print("Pay load: " + str(pay_load))
 
 if pay_load >= max_payload:
     over_weight = True
 
-if pay_load == max_payload:
-    print("You are at max weight.")
-    print("Be careful with high density altitude at Max weight. Have a safe flight and squawk VFR!")
-elif pay_load <= max_payload:
+if pay_load <= max_payload:
+    over_weight = False
     print("You're under weight by " + str(max_payload - pay_load) + " lbs.")
     print("Have a safe flight and squawk VFR!")
+
+if pay_load == max_payload:
+    over_weight = False
+    print("You are at max weight.")
+    print("Be careful with high density altitude at Max weight. Have a safe flight and squawk VFR!")
+
 else:
     if over_weight is True:
-        if pay_load - max_payload >= 200:
+        if pay_load - max_payload >= 150:
             print("You are seriously over weight. Do not go fly until removing " + str(excess_weight) + " lbs." )
         else:
             print("You're OVER weight by " + str(pay_load - max_payload) + " pounds.")

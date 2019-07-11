8a1805ce5b8d2f51da7e56877412767f2677d70e
flyingcoder900 <bryan720b@gmail.com>
2019-07-11 12:44:19 -0700

    THis is just an updated version with slight changes on 7-11

---
+++ TestingZone.py
@@ -0,0 +1,7 @@
+list1 = ["D", "E", "F"]
+list2 = ["A", "B", "C"]
+list3 = []
+for x in list1:
+    for y in list2:
+        list3.append(x + "+" + y + "= Wut you really really want.")
+        print(list3)
--- app.py
+++ app.py
@@ -5,22 +5,33 @@
 plane_models = ["C172M", "PA28R", "C150"]
 plane_payloads = [650, 900, 500]
 
+print("Plane models in Database:")
 
-print("Plane models: " + str(plane_models))
-plane = input("Enter plane ICAO + Model Number : ")
+# for loop to retrieve and display plane models
 
-# This will set the payload if the plane is already in the system. Or add the plane and payload if it is not already.
+for plane_list in plane_models:
+    print(plane_list)
+
+# Request user for aircraft type and convert to uppercase.
+plane = input("Enter plane ICAO + Model Number: ")
+plane = plane.upper()
+
+
+# This will set the payload based off of the correlating payload if the plane is already in the system.
 
 if plane in plane_models:
     max_payload = plane_payloads.pop(plane_models.index(plane))
-    print("Your plane is set as a " + plane + ". With a max payload of " + str(max_payload) )
+    print("Your plane is set as a " + plane + ". With a max payload of " + str(max_payload) + "lbs.")
+
+# Now define new plane if not listed.
+
 if plane not in plane_models:
     print("The plane was not listed!")
     new_plane = plane
     plane_payloads.append(input("What is the payload of this plane: "))
     max_payload = int(plane_payloads[-1])
     plane_models.append(new_plane)
-    print("Your plane is set as a " + plane_models[-1] + ". With a payload of " + str(plane_payloads[-1] ) )
+    print("Your plane is set as a " + plane_models[-1] + ". With a payload of " + str(plane_payloads[-1]) + "lbs.")
 
 # Request User to enter weights for flight.
 
@@ -29,7 +40,7 @@ co_pilot = int(input("Enter Copilot weight: "))
 passengers = int(input("Enter Passengers weight: "))
 fuel = int(input("Enter Gallons: "))
 
-# Add up the weights
+# Add up the weights and define variables. 
 
 fuel_weight = fuel * 6
 over_weight = True
@@ -57,7 +68,7 @@ if pay_load == max_payload:
 else:
     if over_weight is True:
         if pay_load - max_payload >= 150:
-            print("You are seriously over weight. Do not go fly until removing " + str(excess_weight) + " lbs." )
+            print("You are seriously over weight. Do not go fly until removing " + str(excess_weight) + " lbs.")
         else:
             print("You're OVER weight by " + str(pay_load - max_payload) + " pounds.")
             print("You will need to carry " + str(round(fuel_removed)) + " less gallons of fuel.")
